92745728baa40867075e98b9bdf7dd649b51fbd9
lziqi <709124735@qq.com>
2021-05-10 22:00:51 +0800

    feat:添加获取天气

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"github.com/tidwall/gjson"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Weather struct {
+	userId int
+	id     int
+	title  string
+	body   string
+}
+
+func writeStringToFile(text string) {
+	err := ioutil.WriteFile("README.md", []byte(text), 0644)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	fmt.Println("start")
+
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	response, err := http.Get("https://restapi.amap.com/v3/weather/weatherInfo?city=420100&extensions=all&key=a2df40ded51af6989ac101865d0c516b")
+
+	if err != nil {
+		log.Println(err)
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		println(err)
+	}
+
+	city := gjson.Get(string(body), "forecasts.#.city").Array()[0]
+	weather := gjson.Get(string(body), "forecasts.#.casts").Array()[0]
+
+	txt := city.String() + "\n"
+	for _, day := range weather.Array() {
+		date := gjson.Get(day.String(), "date").String()
+		dayweather := gjson.Get(day.String(), "dayweather").String()
+		nightweather := gjson.Get(day.String(), "nightweather").String()
+		daytemp := gjson.Get(day.String(), "daytemp").String()
+		nighttemp := gjson.Get(day.String(), "nighttemp").String()
+		txt += date + " " + dayweather + " " + nightweather + " " + daytemp + " ℃ " + nighttemp + " ℃\n"
+	}
+	writeStringToFile(txt)
+
+	//https://jsonplaceholder.typicode.com/posts/1
+	//https://restapi.amap.com/v3/weather/weatherInfo?city=420100&extensions=all&key=a2df40ded51af6989ac101865d0c516b
+}
